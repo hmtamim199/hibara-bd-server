c5975a697b616d599cf061d173a494113d86afbb
hmtamim199 <hmtamim8906@gmail.com>
2025-02-15 22:23:18 +0600

    server added

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// conection start here
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.HIBARA_USER}:${process.env.HIBARA_PASS}@cluster0.cqjrq2b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const menuCollectuion = client.db("hibaraBD").collection("menu");
+
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollectuion.find().toArray();
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// conection end here
 
 app.get("/", (req, res) => {
   res.send("hibara bd is running successfully");
